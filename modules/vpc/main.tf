e273e09099af58a5ce568bcdbbe6c05ec2c81c76
Jordi Prats <jordi.prats@gmail.com>
2022-04-19 18:52:50 +0200

    improve voc subneting

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,10 +7,11 @@ resource "aws_vpc" "server_vpc" {
 }
 
 resource "aws_subnet" "vpc_subnets" {
-  for_each          = var.az_subnets
-  cidr_block        = each.value
+  for_each          = toset(var.az_subnets)
+
+  cidr_block        = cidrsubnet(var.main_vpc_cidr_block, 8, index(var.az_subnets, each.value))
   vpc_id            = aws_vpc.server_vpc.id
-  availability_zone = each.key
+  availability_zone = each.value
 
   tags = var.tags
 }
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -10,12 +10,12 @@ variable "main_vpc_cidr_block" {
 
 variable "az_subnets" {
   description = "List of AZs to use"
-  type        = map(string)
-  default = {
-    us-west-2a = "10.12.100.0/24",
-    us-west-2b = "10.12.101.0/24",
-    us-west-2c = "10.12.102.0/24"
-  }
+  type        = list(string)
+  default = [
+    "us-west-2a",
+    "us-west-2b",
+    "us-west-2c"
+  ]
 }
 
 variable "tags" {
